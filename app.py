c998b6a2fce90291f4cfa8352059ab8b557c9c9d
Jouni Leino <jouni.leino@siili.com>
2019-10-12 01:54:24 +0200

    Setting debug mode false

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import os
 
+import logging
+from logging.handlers import RotatingFileHandler
+
 from app import app
 
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = False
     host = os.environ.get('HOST', '0.0.0.0')
     port = os.environ.get('PORT', 8080)
     app.run(host=host, port=port)
