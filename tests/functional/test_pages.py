148ad1f16952bcb1eac6017144c8cb1ba7ec3228
Jouni Leino <jouni.leino@siili.com>
2019-11-13 22:00:54 +0100

    Adding page tests

--- tests/functional/test_pages.py
+++ tests/functional/test_pages.py
@@ -11,7 +11,7 @@ def client():
 def test_assert():
     assert True
 
-def test_frontpage(client):
+def test_front_page(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b"Latest news" in response.data
@@ -19,3 +19,37 @@ def test_frontpage(client):
     assert b"Snowboarding" in response.data
     assert b"Login" in response.data
 
+def test_news_page(client):
+    response = client.get('/news')
+    assert response.status_code == 200
+    assert b"News" in response.data
+
+def test_youtube_page(client):
+    response = client.get('/youtube/playlists')
+    assert response.status_code == 200
+    assert b"Playlists" in response.data
+
+def test_facebook_page(client):
+    response = client.get('/facebook/info')
+    assert response.status_code == 200
+    assert b"Facebook" in response.data
+
+def test_guides_page(client):
+    response = client.get('/guides')
+    assert response.status_code == 200
+    assert b"Guides" in response.data
+
+def test_login_page(client):
+    response = client.get('/auth/login')
+    assert response.status_code == 200
+    assert b"Guides" in response.data
+
+def test_register_page(client):
+    response = client.get('/auth/register')
+    assert response.status_code == 200
+    assert b"Guides" in response.data
+
+def test_logout_page(client):
+    response = client.get('/auth/logout', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Latest news" in response.data
