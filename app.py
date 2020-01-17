cd411b5c4859710cf1c37641caffe460072a04ad
Jouni Leino <jouni.leino@siili.com>
2020-01-17 16:04:45 +0100

    Modifying port to 8000

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ from app import app
 if __name__ == '__main__':
     app.debug = False
     host = os.environ.get('HOST', '0.0.0.0')
-    port = os.environ.get('PORT', 8080)
+    port = os.environ.get('PORT', 8000)
     app.run(host=host, port=port)
