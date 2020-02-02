25d797f5c77d0bc94de86a7eb56259139cf8d3b5
Jouni Leino <jouni.leino@siili.com>
2020-02-02 13:42:48 +0100

    Adding image preview feature for upload form

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -2,4 +2,20 @@ function confirmDelete(form) {
     if (confirm("Are you sure you want to delete?")) {
         form.submit();
     }
-}    
+}
+
+function filePreview(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#uploadPreview > img').remove();
+            $('#uploadPreview').append('<img src="'+e.target.result+'" />');
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$(document).ready(function() {
+    $("#file").change(function () {
+        filePreview(this);
+    });
+});
