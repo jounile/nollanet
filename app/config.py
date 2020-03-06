9db39802555ffa690b6f382d05b1d955148d172b
Jouni Leino <jouni.leino@siili.com>
2020-03-06 14:01:55 +0100

    Adding FB comments social plugin

--- app/config.py
+++ app/config.py
@@ -7,6 +7,8 @@ class DefaultConfig(object):
     DEBUG = True
     TESTING = False
 
+    TITLE = 'Nolla.net'
+
     APP_ROOT = os.path.dirname(os.path.abspath(__file__))
     APP_STATIC = os.path.join(APP_ROOT, 'static')
 
