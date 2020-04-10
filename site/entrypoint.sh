1835e7270fc0bdf60ffacfbfe3de81b3035cc365
Jouni Leino <jouni.leino@siili.com>
2020-04-10 09:15:14 +0200

    Adding database creations and seeding

--- site/entrypoint.sh
+++ site/entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 echo "Waiting for database..."
-
 while ! nc -z $MYSQL_HOST $MYSQL_PORT; do
   sleep 0.5
 done
-
 echo "Database started"
 
-#python manage.py flush --no-input
-#python manage.py migrate
-#python manage.py collectstatic --no-input --clear
-#python manage.py createsuperuser --no-input --email admin@example.invalid --username admin
-#python manage.py loaddata accounts.json transactions.json
+echo "Database init"
+python manage.py
+echo "Database init finished"
 
 exec "$@"
