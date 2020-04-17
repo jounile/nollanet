b477f7407b8067be2a230acda8759bda38093acf
Jouni Leino <jouni.leino@siili.com>
2020-04-17 11:38:16 +0200

    Default map in spots center europe

--- app/mod_spots/controllers.py
+++ app/mod_spots/controllers.py
@@ -34,22 +34,22 @@ def all():
     types = MapType.query.order_by(MapType.name.asc())
     spots = MapSpot.query.filter_by(maa_id=selected_maa_id).order_by(MapSpot.paivays.desc())
 
-    map_center_lat = 60.186427
-    map_center_lon = 24.934886
-    map_zoom_level = 5
+    lat = 50.395346
+    lon = 8.667042
+    zoom = 3
 
     if selected_maa_id != 0:
         country = MapCountry.query.filter_by(id=selected_maa_id).first()
-        map_center_lat = country.lat
-        map_center_lon = country.lon
-        map_zoom_level = 3
+        lat = country.lat
+        lon = country.lon
+        zoom = 3
         spots = spots.filter_by(maa_id=selected_maa_id)
 
     if selected_paikkakunta_id != 0:
         town = MapTown.query.filter_by(id=selected_paikkakunta_id).first()
-        map_center_lat = town.lat
-        map_center_lon = town.lon
-        map_zoom_level = 11
+        lat = town.lat
+        lon = town.lon
+        zoom = 11
         spots = spots.filter_by(paikkakunta_id=selected_paikkakunta_id)
 
     if selected_type_id != 0:
@@ -66,10 +66,10 @@ def all():
     # creating a map in the view
     mymap = Map(
         identifier="view-side",
-        style="height:400px;width:1170px;margin:0;",
-        zoom=map_zoom_level,
-        lat=map_center_lat,
-        lng=map_center_lon,
+        style="height:400px;width:100%;margin:0;",
+        zoom=zoom,
+        lat=lat,
+        lng=lon,
         markers=markers
     )
 
